de9e8157d5871e9cd43d729365699c0e7ca5dc96
Ankur Kothari <ankur@cognitiveclouds.com>
2014-09-15 12:55:40 +0530

    add ability to view a single project

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -6,4 +6,10 @@ feature 'Viewing projects' do
     visit root_path
     expect(page).to have_css 'ul.projects', text: 'TextMate 2'
   end
+  scenario "can view a single project" do
+    create(:project, name: "TextMate 2")
+    visit root_path
+    click_link "TextMate 2"
+    expect(page).to have_content "TextMate 2"
+  end
 end
