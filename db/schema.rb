6b25d3c1c111887f504d3abb0f5a16828abaa842
Ankur Kothari <ankur@cognitiveclouds.com>
2014-09-16 13:25:41 +0530

    Make a user model with password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140915101005) do
+ActiveRecord::Schema.define(version: 20140916074229) do
 
   create_table "projects", force: true do |t|
     t.string   "name"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20140915101005) do
     t.string   "title"
     t.text     "description"
     t.integer  "project_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
